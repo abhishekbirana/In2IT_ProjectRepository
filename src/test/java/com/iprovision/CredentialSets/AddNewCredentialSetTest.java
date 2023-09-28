8e1add39b51ebfc6c260fa752572787d6578ddf7
abhishek.birana <abhishek.birana@IN2-NOINB-ABBI.in2ittech.local>
2023-09-28 10:46:15 +0530

    This message is committed by Abhishek Birana

--- src/test/java/com/iprovision/CredentialSets/AddNewCredentialSetTest.java
+++ src/test/java/com/iprovision/CredentialSets/AddNewCredentialSetTest.java
@@ -40,6 +40,7 @@ public class AddNewCredentialSetTest extends BaseClass {
 		csp.enterAllRequiredDetails(driver, transType, name, username, password, portNo, timeOut);
 		
 		// Step 5 : Validate
+		System.out.println("Hiii");
 		String message = csp.getValidationMessage();
 		System.out.println(message+" --- Credential Set is Created Sucessfully --- ");
 		csp.selectAnyCredentialSet(driver);
